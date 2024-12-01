c4509542ccef1b9a8b876f03e64094b2d756d9cf
mahmoudamr <mahmoud.mansour.amr.mm@gmail.com>
2024-12-01 02:57:33 +0200

    meeting routes

--- server/controllers/meeting/_routes.js
+++ server/controllers/meeting/_routes.js
@@ -1,5 +1,18 @@
-const express = require('express');
-
+const express = require("express");
 const router = express.Router();
+const authenticate = require("../../middelwares/auth");
+const {
+  add,
+  index,
+  view,
+  deleteData,
+  deleteMany,
+} = require("../../controllers/meeting/meeting");
 
-module.exports = router
+router.get("/view", authenticate, index);
+router.post("/", authenticate, add);
+router.get("/:id", authenticate, view);
+router.delete("/:id", authenticate, deleteData);
+router.delete("/", authenticate, deleteMany);
+
+module.exports = router;
