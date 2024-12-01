66deeb6d40176f7fcb96cefbc3c83fc620842892
mahmoudamr <mahmoud.mansour.amr.mm@gmail.com>
2024-12-01 02:59:17 +0200

    FIx login bug

--- server/middelwares/auth.js
+++ server/middelwares/auth.js
@@ -1,18 +1,33 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    const token = req.headers.authorization;
+  const authHeader = req.headers.authorization;
 
-    if (!token) {
-        res.status(401).json({ message: "Authentication failed , Token missing" });
-    }
-    try {
-        const decode = jwt.verify(token, 'secret_key')
-        req.user = decode
-        next();
-    } catch (err) {
-        res.status(500).json({ message: 'Authentication failed. Invalid token.' })
-    }
-}
+  if (!authHeader) {
+    return res
+      .status(401)
+      .json({ message: "Authentication failed, Token missing" });
+  }
 
-module.exports = auth
+  const token = authHeader.split(" ")[1];
+  if (!token) {
+    return res
+      .status(401)
+      .json({ message: "Authentication failed, Token missing." });
+  }
+
+  try {
+    const decode = jwt.verify(token, "secret_key");
+    req.user = decode;
+    next();
+  } catch (err) {
+    return res
+      .status(401)
+      .json({
+        message: "Authentication failed. Invalid token.",
+        error: err.message,
+      });
+  }
+};
+
+module.exports = auth;
